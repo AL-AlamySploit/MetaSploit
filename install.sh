7df3d85a90482b99bfa8656f673e91dccc3ab06e
AL-AlamySploit <59588007+AL-AlamySploit@users.noreply.github.com>
2020-03-18 16:39:06 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+A7Y='\033[01;32m'
+printf "${A7Y}[+] witing to Down..  ..\n"
+$ apt update && apt upgrade -y
+
+apt install git
+apt install python3
+apt install python
+apt install ruby
+apt install wget
+apt install unzip
+apt install zip
+apt install nano
+apt install perl
+apt install curl
+apt install proot
+apt install python2
+apt install ruby
+apt install wget
+apt install curl
+echo " $A7Y [+] Done..!"
